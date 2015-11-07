cab4a64ec6d9787da03ff907f6639bf60906fe8a
sgenou1 <sgenou1@gmail.com>
2015-11-07 11:14:58 -0500

    Suite

--- merchants.php
+++ merchants.php
@@ -8,38 +8,22 @@ foreach ($merchants_list as $merchants_list_element => $merchants_list_realname)
   //echo 'execute @e[type=Squid,name=' . $merchants_list_element . '] ~ ~ ~ blockdata ~ ~ ~ {Text1:"{\"text\":\" \",\"color\":\"dark_red\",\"bold\":\"true\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/scoreboard players set @p ' . $merchants_list_element . ' 1\"}}",Text2:"{\"text\":\"Appel\",\"color\":\"dark_red\",\"bold\":\"true\"}",Text3:"{\"text\":\"Marchand\",\"color\":\"dark_red\",\"bold\":\"true\"}",Text4:"{\"text\":\"\",\"color\":\"red\"}",id:"Sign"}';
   echo "\r\n";
   //echo 'execute @e[type=Squid,name=' . $merchants_list_element . '] ~ ~ ~ tp @e[type=Squid,r=1] ~ -1000 ~';
-  echo '			{
+  echo '{
 				maxUses:1000000,
-				buy:
-				{
-					id:diamond,
-					Count:1
-				},
-				sell:
-				{
+				buy:{id:diamond,Count:1},
+				sell:{
 					id:spawn_egg,
 					Count:1,
 					Damage:94,
 					tag:
 					{
-						ench:
-						[
-							{
-								id:34,
-								lvl:1
-							}
-						],
+						ench:[{id:34,lvl:1}],
 						display:
 						{
 							Name:' . $merchants_list_element . ',
 							Lore:
-							[
-								TP 8 blocks plus haut
-							]
-						}
-					}
-				}
-			},
+							[Appel ' . $merchants_list_realname .']}}}		},
 ';
+  echo '{maxUses:1000000,buy:{id:diamond,Count:16},sell:{id:spawn_egg,Count:1,Damage:94,tag:{ench:[{id:34,lvl:1}],display:{Name:' . $merchants_list_element . '}}}},';
   echo "\r\n";  
 }
