0ca4dd23ff248b89f295aa5af10833348b648f19
sgenou1 <sgenou1@gmail.com>
2015-11-07 09:33:52 -0500

    Suite...

--- merchants.php
+++ merchants.php
@@ -4,5 +4,6 @@ $merchants_list = array('spawnAlchimiste', 'spwnMineMerchant', 'spwnClayMerchant
                         'spwnPrimeur', 'spwnBoucher', 'spwnMarchevo', 'spwnRaidstone', 'spwnFleuriste', 'spwnDrugStore',
                         'spwnBarman', 'spwnServSalThe', 'spwnTPMerchant', 'spwnPrismarin', 'spwnPancartier');
 foreach ($merchants_list as $merchants_list_element) {
-  
+
+  echo 'execute @e[type=Squid,name=' . $merchants_list_element . '] ~ ~ ~ blockdata ~ ~ ~ {Text1:"{\"text\":\" \",\"color\":\"dark_red\",\"bold\":\"true\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/scoreboard players set @a ' . $merchants_list_element . ' 1\"}}",Text2:"{\"text\":\"|\",\"color\":\"dark_red\",\"bold\":\"true\"}",Text3:"{\"text\":\"V\",\"color\":\"dark_red\",\"bold\":\"true\"}",Text4:"{\"text\":\"\",\"color\":\"red\"}",id:"Sign"}';
 }
