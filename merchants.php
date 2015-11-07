8b8797ecdb8839eb8ca4815d0d1dd81c1a22b2d7
sgenou1 <sgenou1@gmail.com>
2015-11-07 09:18:37 -0500

    Saisie de la liste des marchands

---
+++ merchants.php
@@ -0,0 +1,8 @@
+<?php
+$merchants_list = array('spawnAlchimiste', 'spwnMineMerchant', 'spwnClayMerchant', 'spwnPetMerchant', 'spwnTurdMerchant',
+                        'spwnDyeMerchant', 'spwnBanker', 'spwnBanker2', 'spwnBaker', 'spwnArmurier', 'spwnPoissonier',
+                        'spwnPrimeur', 'spwnBoucher', 'spwnMarchevo', 'spwnRaidstone', 'spwnFleuriste', 'spwnDrugStore',
+                        'spwnBarman', 'spwnServSalThe', 'spwnTPMerchant', 'spwnPrismarin', 'spwnPancartier');
+foreach ($merchants_list as $merchants_list_element) {
+  
+}
