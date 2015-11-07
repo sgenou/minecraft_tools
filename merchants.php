0fb6583cf404904a6be98b071646d6bfa0e13aa7
sgenou1 <sgenou1@gmail.com>
2015-11-07 09:35:04 -0500

    Suite...

--- merchants.php
+++ merchants.php
@@ -5,5 +5,5 @@ $merchants_list = array('spawnAlchimiste', 'spwnMineMerchant', 'spwnClayMerchant
                         'spwnBarman', 'spwnServSalThe', 'spwnTPMerchant', 'spwnPrismarin', 'spwnPancartier');
 foreach ($merchants_list as $merchants_list_element) {
 
-  echo 'execute @e[type=Squid,name=' . $merchants_list_element . '] ~ ~ ~ blockdata ~ ~ ~ {Text1:"{\"text\":\" \",\"color\":\"dark_red\",\"bold\":\"true\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/scoreboard players set @a ' . $merchants_list_element . ' 1\"}}",Text2:"{\"text\":\"|\",\"color\":\"dark_red\",\"bold\":\"true\"}",Text3:"{\"text\":\"V\",\"color\":\"dark_red\",\"bold\":\"true\"}",Text4:"{\"text\":\"\",\"color\":\"red\"}",id:"Sign"}';
+  echo 'execute @e[type=Squid,name=' . $merchants_list_element . '] ~ ~ ~ blockdata ~ ~ ~ {Text1:"{\"text\":\" \",\"color\":\"dark_red\",\"bold\":\"true\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/scoreboard players set @a ' . $merchants_list_element . ' 1\"}}",Text2:"{\"text\":\"Appel\",\"color\":\"dark_red\",\"bold\":\"true\"}",Text3:"{\"text\":\"Marchand\",\"color\":\"dark_red\",\"bold\":\"true\"}",Text4:"{\"text\":\"\",\"color\":\"red\"}",id:"Sign"}';
 }
