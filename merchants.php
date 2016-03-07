c931fe9fa723464181b93014d84cc580d38fc278
sgenou <sgenou1@gmail.com>
2016-03-07 20:48:01 +0000

    Debut de création du marchand d'enchantements

--- merchants.php
+++ merchants.php
@@ -10,7 +10,3 @@ foreach ($merchants_list as $merchants_list_element => $merchants_list_realname)
 	echo '{maxUses:1000000,buy:{id:diamond,Count:16},sell:{id:spawn_egg,Count:1,Damage:94,tag:{ench:[{id:34,lvl:1}],display:{Name:' . $merchants_list_element . ',Lore:[Appel ' . $merchants_list_realname . ']}}}},';
 	echo "\r\n";
 }
-echo("this is another test");
-echo("o hai");
-	echo('yeah');
-	
